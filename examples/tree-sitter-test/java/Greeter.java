f2bacdb6e3aa4b62e160786a6fdffa8a4cc98d37
Kiyoka Nishiyama <kiyokasumibi@gmail.com>
2026-03-18 22:57:39 +0900

    Add tree-sitter test examples (Java and Python, small and large)

---
+++ examples/tree-sitter-test/java/Greeter.java
@@ -0,0 +1,13 @@
+package com.example.hello;
+
+public class Greeter {
+    private String name;
+
+    public Greeter(String name) {
+        this.name = name;
+    }
+
+    public String greet() {
+        return "Hello, " + name + "!";
+    }
+}
